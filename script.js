a7c3b8b57edde0200200c8ba191a2ce69e135437
manojn2207 <112647668+manojn2207@users.noreply.github.com>
2024-07-11 12:51:03 -0400

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+function phoneNumber(value){
+    // const pattern = /^[+]{1}(?:[0-9\-\\(\\)\\/.]\s?){6,15}[0-9]{1}$/;
+    // if (!pattern.test(value)) {
+    //     alert("enter valid phone numeber");
+    // } 
+}
